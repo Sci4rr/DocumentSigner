280794ff08e2753bafdef179450cd88c55857db6
Sci4rr <163331503+Sci4rr@users.noreply.github.com>
2024-05-01 18:35:24 +0200

    Add file auth.go

---
+++ auth.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "time"
+
+    "github.com/dgrijalva/jwt-go"
+    "github.com/golang-jwt/jwt/v4"
+    "github.com/gorilla/mux"
+    "golang.org/x/crypto/bcrypt"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+type Credentials struct {
+    Password string `json:"password"`
+    Username string `json:"username"`
+}
+
+type Claims struct {
+    Username string `json:"username"`
+    jwt.StandardClaims
+}
+
+var users = map[string]string{}
+
+func main() {
+    router := mux.NewRouter()
+
+    router.HandleFunc("/register", registerHandler).Methods("POST")
+    router.HandleFunc("/login", loginHandler).Methods("POST")
+    router.HandleFunc("/welcome", authMiddleware(welcomeHandler)).Methods("GET")
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+    var creds Credentials
+    err := json.NewDecoder(r.Body).Decode(&creds)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    users[creds.Username] = string(hashedPassword)
+
+    w.WriteHeader(http.StatusCreated)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    var creds Credentials
+    err := json.NewDecoder(r.Body).Decode(&creds)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    expectedPassword, ok := users[creds.Username]
+
+    if !ok || bcrypt.CompareHashAndPassword([]byte(expectedPassword), []byte(creds.Password)) != nil {
+        w.WriteHeader(http.StatusUnauthorized)
+        return
+    }
+
+    expirationTime := time.Now().Add(5 * time.Minute)
+    claims := &Claims{
+        Username: creds.Username,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: expirationTime.Unix(),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    tokenString, err := token.SignedString(jwtKey)
+
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    http.SetCookie(w, &http.Cookie{
+        Name:    "token",
+        Value:   tokenString,
+        Expires: expirationTime,
+    })
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(fmt.Sprintf("Welcome!")))
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cookie, err := r.Cookie("token")
+        if err != nil {
+            if err == http.ErrNoCookie {
+                w.WriteHeader(http.StatusUnauthorized)
+                return
+            }
+            w.WriteHeader(http.StatusBadRequest)
+            return
+        }
+
+        tokenStr := cookie.Value
+        claims := &Claims{}
+
+        token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+            return jwtKey, nil
+        })
+
+        if err != nil {
+            if err == jwt.ErrSignatureInvalid {
+                w.WriteHeader(http.StatusUnauthorized)
+                return
+            }
+            w.WriteHeader(http.StatusBadRequest)
+            return
+        }
+
+        if !token.Valid {
+            w.WriteHeader(http.StatusUnauthorized)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
