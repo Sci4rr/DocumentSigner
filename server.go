0b08a1b0ffd5786b36ee035557a26d61b52aa7ff
Sci4rr <163331503+Sci4rr@users.noreply.github.com>
2024-05-07 16:23:26 +0200

    optimize the naming

--- server.go
+++ server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+    "fmt"
     "github.com/dgrijalva/jwt-go"
+    "github.com/gin-gonic/gin"
+    "golang.org/x/crypto/bcrypt"
+    "net/http"
     "time"
 )
 
@@ -18,30 +22,37 @@ func main() {
 func Login(c *gin.Context) {
     var loginCreds User
     if err := c.BindJSON(&loginCreds); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Failed to login: %v", err)})
+        respondWithError(c, http.StatusBadRequest, fmt.Sprintf("Failed to login: %v", err))
         return
     }
 
-    storedPassword, exists := Users[loginCreds.Username]
-    if !exists || bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(loginCreds.Password)) != nil {
-        c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+    if storedPassword, exists := Users[loginCreds.Username]; !exists || bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(loginCreds.Password)) != nil {
+        respondWithError(c, http.StatusUnauthorized, "Invalid username or password")
         return
     }
 
+    tokenString, err := generateToken(loginCreds.Username)
+    if err != nil {
+        respondWithError(c, http.StatusInternalServerError, "Failed to create token")
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"token": tokenString, "message": "Login successful"})
+}
+
+func respondWithError(c *gin.Context, code int, message string) {
+    c.JSON(code, gin.H{"error": message})
+}
+
+func generateToken(username string) (string, error) {
     expirationTime := time.Now().Add(5 * time.Minute)
     claims := &Claims{
-        Username: loginCreds.Username,
+        Username: username,
         StandardClaims: jwt.StandardClaims{
             ExpiresAt: expirationTime.Unix(),
         },
     }
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    tokenString, err := token.SignedString(jwtKey)
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create token"})
-        return
-    }
-
-    c.JSON(http.StatusOK, gin.H{"token": tokenString, "message": "Login successful"})
+    return token.SignedString(jwtKey)
 }
